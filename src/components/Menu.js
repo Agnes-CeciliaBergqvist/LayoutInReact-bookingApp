b07ac251b50c551755dc9a7463bfe8281da8d4bc
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-04-19 16:56:35 +0200

    starting on header menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import {Link} from "react-router-dom"; 
+import React from "react";
+import { Link } from "react-router-dom";
+// import 'https://fontawesome.com/';
 
 function Menu() {
-    return (
-        <div>
+  return (
+    <div>
+      <nav class="bg-grey-100">
+        <div class="max-w-6x1 mx-auto border border-red-400">
+          <div class="flex justify-between">
+            {/* <div class="flex"> */}
+              <span>Last minute travel</span>
+              <button>
+                <i class="material-icons">menu</i>
+              </button>
 
-                <nav>
-                    <Link to="/">CardList</Link>
-                    <Link to="/form">Lägg till</Link>
-                </nav>
-            
+              <Link to="/">CardList</Link>
+              <Link to="/form">Lägg till</Link>
+              <Link to="/">My trips</Link>
+              <button class="py-2 px-3 bg-yellow-400 text-yellow-900 rounded">Login</button>
+            {/* </div> */}
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
