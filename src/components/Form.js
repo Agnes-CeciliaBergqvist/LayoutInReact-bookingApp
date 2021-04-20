a2c4d819db5e2e817d5e920cd4327a44359825d4
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-04-20 15:01:00 +0200

    started on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -27,22 +27,39 @@ function onChange(e) { 
 }
 
     return(
-        <div>
-           <form onSubmit={onSubmit}>
+        // <div>
+        //    <form onSubmit={onSubmit}>
 
-               <input value={formValues.destination} name="destination" onChange={onChange} />
-               <input type="number" name="price" value={formValues.price} onChange={onChange} />
+        //        <input value={formValues.destination} name="destination" onChange={onChange} />
+        //        
 
-               <button>Add</button>
+        //       
 
                
 
-           </form>
+        //    </form>
 
 
 
 
-        </div>
+        // </div>
+
+<div>
+    <form onSubmit={onSubmit}>
+<label for="price" class="block text-sm font-medium text-gray-800">Please enter your details:</label>
+<div class="mt-1 relative rounded-md shadow-sm">
+  
+  <input value={formValues.destination} name="destination" onChange={onChange}  class="focus:ring-indigo-500 focus:border-indigo-500 block pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Firstname"/>
+  <input type="number" name="price" value={formValues.price} onChange={onChange} class="focus:ring-indigo-500 focus:border-indigo-500 block pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Lastname" />
+
+  <button>Add</button>
+</div>
+</form>
+</div>
+
+       
+  
+
     )
 }
 
