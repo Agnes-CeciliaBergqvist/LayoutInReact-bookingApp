c4f9de0473d55eeac072feaa33831a12aeedb9cb
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-04-29 13:33:48 +0200

    added size for img

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ function Card({ tripDestination, tripDescription, tripPrice, tripImage }) {
     <div className="bg-gray-200 rounded-xl p-6 m-4">
       <h2 className="text-2xl font-bold mb-2">{tripDestination}</h2>
       <div className="text-gray-800 leading-relaxed mb-6">{tripDescription}</div>
-      <img src={"http://localhost:1337" + tripImage.url} alt="images from the database strapi"/>
-      <div className="text-gray-800 leading-relaxed mb-6">{tripPrice}</div>
+      <img className=" w-72 h-52" src={"http://localhost:1337" + tripImage.url} alt="images from the database strapi"/>
+      <div className="text-gray-800 leading-relaxed mb-6">Total price from: {tripPrice}:-</div>
       <button className="text-indigo-500 px-4 py-3 bg-gray-300 rounded hover:bg-indigo-500 hover:text-white transition duration-300">
         Book
       </button>
