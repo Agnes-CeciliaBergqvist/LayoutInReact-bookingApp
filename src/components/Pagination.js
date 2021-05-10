3f676de90d4c1ed3bc56cb1c12ae7c0cbabb437d
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-05-10 17:37:03 +0200

    added design to pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+
 
 function Pagination({ tripsPerPage, totalTrips, paginate }) {
 
@@ -10,21 +11,76 @@ function Pagination({ tripsPerPage, totalTrips, paginate }) {
         pageNumbers.push(i); //Här pushar vi arrayen in i indesxnumret 
 
     }
-    return (
+    
+       
+
+
+  return (
+    
+    <div class="bg-white py-3 absolute bottom-0 w-full border-t border-gray-200 sm:px-6">
+      <div class="flex-1 flex justify-between sm:hidden">
+        <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
+          Previous
+        </a>
+        <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
+          Next
+        </a>
+      </div>
+      <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+       
         <div>
-             <ul className=" flex justify-center pagination">
+          <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+            <a href="#" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+              <span class="sr-only">Previous</span>
+             
+              <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+              </svg>
+            </a>
+            <ul className="flex">
              {pageNumbers.map(number => (
-                 <li key={number} className="page-item"> 
-                 <a onClick={() => paginate(number)}  className="page-link">
+                 <li key={number} className=""> 
+                 <a onClick={() => paginate(number)}  className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-300">
                      {number}
                  </a>
                 </li>
 
              ))}
              </ul>
-            
+                       
+            <a href="#" class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+              <span class="sr-only">Next</span>
+             
+              <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+              </svg>
+            </a>
+          </nav>
         </div>
-    )
+      </div>
+    </div>
+    
+    
+    
+  )
 }
 
+
+
+        
+ 
 export default Pagination; 
+{/* <div className="">
+             <div class="">
+             <ul className="pagination">
+             {pageNumbers.map(number => (
+                 <li key={number} className=""> 
+                 <a onClick={() => paginate(number)}  className="">
+                     {number}
+                 </a>
+                </li>
+
+             ))}
+             </ul>
+             </div> 
+        </div> */}
