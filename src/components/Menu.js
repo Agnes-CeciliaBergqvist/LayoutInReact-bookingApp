6ee29b4c873382ab0fb3a01649e4d274c91039b7
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-05-26 12:56:27 +0200

    added path to homepage

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -24,7 +24,7 @@ function Menu() {
         <nav className="bg-white h-20 relative flex flex-wrap items-center justify-between px-2 py-3 bg-amber-500 mb-3">
           <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-              <Link to="/homepage">
+              <Link to="/">
                 <svg
                   className="mx-auto h-10 w-auto absolute flex left-8"
                   xmlns="http://www.w3.org/2000/svg"
@@ -354,7 +354,7 @@ function Menu() {
                 <li className="nav-item">
                   <Link
                     onClick={() => setNavbarOpen(!navbarOpen)}
-                    to="/homepage"
+                    to="/"
                     className="hover:bg-gray-700 hover:text-white px-3 py-2 m-1 flex items-center text-xs uppercase font-bold leading-snug"
                     aria-current="page"
                   >
