b5081bbef4e8ecf905dffe30042a292c7867ff91
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-06-11 11:24:18 +0200

    cleaned code and added profile page to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -146,10 +146,11 @@ function Menu() {
               id="example-navbar-danger"
             >
               <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                <li className="nav-item">
+
+              <li className="nav-item">
                   <Link
                     onClick={() => setNavbarOpen(!navbarOpen)}
-                    to="/mytrips"
+                    to="/getusers"
                     className="hover:bg-gray-700 hover:text-white px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug"
                     aria-current="page"
                   >
@@ -165,6 +166,30 @@ function Menu() {
                         clipRule="evenodd"
                       />
                     </svg>
+                    Profile
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    onClick={() => setNavbarOpen(!navbarOpen)}
+                    to="/mytrips"
+                    className="hover:bg-gray-700 hover:text-white  px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug"
+                    aria-current="page"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  
                     My bookings
                   </Link>
                 </li>
