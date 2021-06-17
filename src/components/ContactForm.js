407c2fbe65f9f792f08b89356b55c693b80a97dc
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-06-17 11:43:18 +0200

    Email done with EmailJS

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,65 +1,49 @@
-import React, {useState} from "react"; 
+import React from "react";
+import emailjs from "emailjs-com"; 
 
-const ContactForm = () => {
+export default function ContactForm () {
 
-    const [status, setStatus] = useState("Submit"); 
+    function handleSubmit(e) {
+        e.preventDefault();
     
+        emailjs.sendForm('service_ns96466', 'template_o5axqxu', e.target, 'user_9gOxTrmVgSnv3d0uiUzRP')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      }
     
-    
-    const handleSubmit = async (e) => { 
-        e.preventDefault(); 
-
-        setStatus("Sending..."); 
-        const { name, email, message} = e.target.elements; 
-
-        let details = { 
-            name: name.value, 
-            email: email.value, 
-            message: message.value
-        }
-
-        let response = await fetch("http://localhost:5000/contact", { 
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json;charset=utf-8",
-
-        }, 
-        body: JSON.stringify(details), 
-        })
-
-        setStatus("Submit");
-        let result = await response.json();
-        alert(result.status);
-  };
-
-
-
-
-
 
 
     return (
         <div className="h-screen">
             <div className="min-h-screen bg-gray-100 p-0 sm:p-12">
                 <form className="mx-auto max-w-md px-6 py-12 bg-white border-0 shadow-lg sm:rounded-3xl" onSubmit={handleSubmit}>
-                <h1 className="text-2xl font-bold mb-8">CONTACT US</h1>
+
+                    <h1 className="text-2xl font-bold mb-8">CONTACT US</h1>
                     <div>
-                        
                         <input type="text" name="name" placeholder="Name" required 
                         className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"/>
                     </div>
-                    <div>
-                   
-                    <input type="email" name="email" placeholder="Email" required
-                     className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" />
-                    </div>
-                    <div>
-                     
-                        <textarea placeholder="Enter your message" required 
-                        className="h-52 pt-3 pb-2 block w-full px-0 mt-2 bg-transparent border-2 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"/>
-                 </div>
-                 <button className="bg-yellow-200 border-5 rounded-md mt-4 p-1 text-gray-700"type="submit">{status}</button>
 
+                    <div>
+                        <input type="email" name="email" placeholder="Email" required
+                        className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" />
+                    </div>
+
+                    <div>
+                        <input type="subject" name="subject" placeholder="Subject" required
+                        className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" />
+                   </div>
+                    
+                    <div>
+                        <textarea placeholder="Enter your message" name="message" required 
+                        className="h-52 pt-3 pb-2 block w-full px-0 mt-2 bg-transparent border-2 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"/>
+                    </div>
+
+                    <button className="bg-yellow-200 border-5 rounded-md mt-4 p-1 text-gray-700" type="submit" value="Send message">Send</button>
                 </form>
             </div>
         </div>
@@ -68,6 +52,5 @@ const ContactForm = () => {
     )
 }
 
-export default ContactForm; 
 
 
